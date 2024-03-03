2048c153c71a6d38a42cbcb1ab1f75296db4382b
ChengTing <aaronzhan0906@gmail.com>
2024-03-03 21:16:48 +0800

    Version 6

--- script.js
+++ script.js
@@ -7,15 +7,67 @@ const buttonStart = document.getElementById("button-start");
 const buttonStop = document.getElementById("button-stop");
 const table = document.getElementById("table-done-list");
 const inputText = document.getElementById("input-focus-item");
+const example = document.getElementById("example");
 
 let interval; //store variable of timer
 
-//以下是localstorage
+let itemState = []; //store the focus item
 
+// 如果 tableState 沒有項目，則加入示意 table row
+function removeInitialSpan() {
+  if (tableState.length === 0) {
+    example.innerHTML = `
+      <tr>
+        <td>示意</td>
+        <td>25:00</td>
+        <td><button type="button" class="deletebutton">刪除</button></td>
+      </tr>
+    `;
+  }
+}
+
+//以下是localstorage
 let tableState = [];
 
-//以下是計時器
-// timer display
+const STATE_KEY = "tableState";
+
+// Load the state from local storage
+function loadState() {
+  const savedState = localStorage.getItem(STATE_KEY);
+  if (savedState) {
+    return JSON.parse(savedState);
+  } else {
+    return [];
+  }
+}
+
+function initState() {
+  tableState = loadState();
+
+  // 將 tableState 中的項目加入 table
+  tableState.forEach((item) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<td id="newtr">${item.text}</td><td>${
+      item.minutes < 10 ? "0" + item.minutes : item.minutes
+    }:${
+      item.seconds < 10 ? "0" + item.seconds : item.seconds
+    }</td><td><button class="deletebutton">刪除</button></td>`;
+    table.appendChild(tr);
+  });
+}
+
+// Load the state when the page is loaded
+window.onload = function () {
+  initState();
+  removeInitialSpan();
+};
+
+// Save the state to local storage
+function saveState() {
+  localStorage.setItem(STATE_KEY, JSON.stringify(tableState));
+}
+
+//以下是 timer display
 const timer = () => {
   seconds++;
 
@@ -39,10 +91,9 @@ const timer = () => {
     }
   }
 };
-//以上是計時器
 
-//以下是 Focus Start 和 Focus Stop 的功能
-//輸入專注項目，並按下 Focus Start 時，將專注項目加入tableState
+//以下是 Focus Start 和 Focus Stop 功能
+//輸入專注項目，並在按下 Focus Start 時，將專注項目加入 itemState
 buttonStart.addEventListener("click", startFocusTimer);
 function startFocusTimer() {
   const input = document.getElementById("input");
@@ -54,20 +105,21 @@ function startFocusTimer() {
   } else {
     input.value = "";
   }
+
   // 開始計時器
   clearInterval(interval);
   interval = setInterval(timer, 1000);
 
-  //將專注項目加入tableState
-  tableState.unshift(text);
+  // 將專注項目加入 itemState
+  itemState.unshift(text);
 
-  //隱藏輸入框、隱藏 Focus Start、顯示 Focus Stop 按鈕
-  buttonStart.style.display = "none"; // 隱藏 Start 按鈕
-  buttonStop.style.display = "inline-block"; // 顯示 Focus Stop 按鈕
-  inputText.style.display = "none"; // 隱藏 input-focus-item
+  // 隱藏輸入框、隱藏 Focus Start、顯示 Focus Stop 按鈕
+  buttonStart.style.display = "none";
+  buttonStop.style.display = "inline-block";
+  inputText.style.display = "none";
 }
 
-//按下 Focus Stop 時，將專注項目、時間、刪除按鈕加入 Done List
+// 按下 Focus Stop 時，將專注項目、計時器時間、刪除按鈕加入 Done List
 buttonStop.addEventListener("click", stopFocusTimer);
 function stopFocusTimer() {
   clearInterval(interval); // 清除現有計時器，避免計時器運作
@@ -82,50 +134,63 @@ function stopFocusTimer() {
   displaySeconds.innerHTML = `00`;
   displayMinutes.innerHTML = `00`;
 
-  //移除 id initial-scale 的 tr 範例
-  const initialSpan = document.getElementById("initial-span");
-  if (initialSpan) {
-    initialSpan.remove();
+  // 移除 id = example 的範例 table row
+  if (example) {
+    example.remove();
   }
 
   // 取得專注項目
-  const text = tableState.pop();
+  const text = itemState.pop();
 
-  // 創建新的時間顯示
-  const newTime = document.createElement("span");
-  newTime.innerHTML = `${
-    stoppedMinutes < 10 ? "0" + stoppedMinutes : stoppedMinutes
-  }:${stoppedSeconds < 10 ? "0" + stoppedSeconds : stoppedSeconds}`;
+  // 清空 itemState
+  itemState = [];
 
   // 創建刪除鍵
   const newButton = document.createElement("button");
   newButton.classList.add("deletebutton");
   newButton.innerHTML = "刪除";
 
-  // 創建新的 table row 並將項目、時間和按鈕添加到其中
+  // 創建新的 table row 並將項目、計時器時間、按鈕添加到其中
   const tr = document.createElement("tr");
-  tr.innerHTML = `<td>${text}</td><td>${newTime.innerHTML}</td><td>${newButton.outerHTML}</td>`;
+  tr.innerHTML = `<td id="newtr">${text}</td><td>${
+    stoppedMinutes < 10 ? "0" + stoppedMinutes : stoppedMinutes
+  }:${stoppedSeconds < 10 ? "0" + stoppedSeconds : stoppedSeconds}</td><td>${
+    newButton.outerHTML
+  }</td>`;
 
   // 將新的 table row 添加到 table 中
   table.appendChild(tr);
 
   // 顯示 Focus Start 按鈕、隱藏 Focus Stop 按鈕、顯示 input-focus-item
-  buttonStop.style.display = "none"; // 隱藏 Focus Stop 按鈕
-  buttonStart.style.display = "inline-block"; // 顯示 Start 按鈕
-  inputText.style.display = "block"; // 顯示 input-focus-item
+  buttonStop.style.display = "none";
+  buttonStart.style.display = "inline-block";
+  inputText.style.display = "block";
+
+  // 將專注項目、計時器時間和刪除按鈕加入 tableState
+  if (tr) {
+    tableState.unshift({
+      text: text,
+      minutes: stoppedMinutes,
+      seconds: stoppedSeconds,
+    });
+    saveState();
+  } else {
+    return;
+  }
 }
 
-// Done List 的刪除按鈕效果
+// Done List 的 deletebutton 效果
 table.addEventListener("click", function (event) {
-  console.log("deletetest");
   if (event.target.classList.contains("deletebutton")) {
     const row = event.target.closest("tr");
     row.remove();
+
+    // 從 tableState 中刪除對應的項目
+    const text = row.querySelector("td").innerText;
+    const index = tableState.findIndex((item) => item.text === text);
+    if (index !== -1) {
+      tableState.splice(index, 1); // 只刪除對應的項目
+      saveState();
+    }
   }
 });
-
-//預防刷新頁面（最後再來檢查需不需要這一段）
-// const form = document.getElementById("input-wrapper");
-// form.addEventListener("submit", (event) => {
-//   evenpreventDefault();
-// });
